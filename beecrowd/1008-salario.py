72a9f5b7bb1a92a42000932f81f3c87c9a07a58d
skrcruz <20261ctb0100038@estudantes.ifpr.edu.br>
2026-04-09 19:42:29 +0000

    Aula 11 - 1008: salario

---
+++ beecrowd/1008-salario.py
@@ -0,0 +1,24 @@
+'''
+problema 1008 beecrowd
+Estudantes: Samuel Ribeiro da Cruz
+Data; 2026.04.09
+'''
+
+#Objetivo Calcular o sálario de um funcionario com as horas e sálario por hora e registrar seu número.
+
+# Analise LIAC
+#entrada:
+#Processamento:
+#saída:
+
+N = int(input(""))
+#Pega o número do funcionario
+H = int(input(""))
+#pega as horas trabalhadas do funcionario
+S = float(input(""))
+#recebe o salario por hora
+
+print(f"NUMBER = {N}")
+#mostra o número do fúncionario
+print(f"SALARY = U$ {H * S:.2f}")
+#mostra o sálario já calculado
