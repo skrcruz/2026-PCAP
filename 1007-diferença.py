444aab688bc0c6afa2e5a9d674d871adb4408e2a
skrcruz <20261ctb0100038@estudantes.ifpr.edu.br>
2026-04-16 19:44:40 +0000

    1007 diferença.py

---
+++ 1007-diferença.py
@@ -0,0 +1,24 @@
+'''
+problema 1007 beecrowd
+data: 26.04.16
+estudante: Samuel Ribeiro da Cruz
+'''
+
+#obejtivo: calcular a diferença dos numeros inteiros
+
+#analise LIAC
+#entrada: os quatro valores A, B, C, D
+#saída:DIFERENÇA = dif
+#operadores:a diferença de A*B e C*D
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+# são as entradas que serão calculadas as diferenças
+
+dif = (A*B) - (C*D)
+#calcula a diferença dos valores inteiros que entraram
+
+print(f"DIFERENCA = {dif}")
+#mostra a diferença2
