6fb211e4ee2672b7c1965cfe47d4572afd4a1165
skrcruz <20261ctb0100038@estudantes.ifpr.edu.br>
2026-05-28 20:01:54 +0000

    adivinhe2.py

--- aula 16/adivenhe.py
+++ aula 16/adivenhe.py
@@ -1,7 +1,7 @@
 #disciplina : Pensamento Computacional, Algoritimos e Programação (PCAP)
 #Projeto : Jogo "Adivinhe o número" 
 #Arquivo : adivinhe.py
-# autor : Samuel RIbeiro da Cru
+#autor : Samuel RIbeiro da Cru
 #data : 28/05/2026
 
 import random
@@ -12,6 +12,10 @@ numero_secreto = random.randint(1, 10)
 #pedimos um palpite
 palpite = int(input("Digite um número de 1 a 10:"))
 
-#mostramos o resultado do primeiro teste
-print("Você chutou:", palpite)
-print("O número ecreto era:", numero_secreto)
+#comparamos o palpite com o número secreto
+if palpite == numero_secreto:
+    print("Acertou! O número era", numero_secreto)
+elif palpite < numero_secreto:
+    print("Muito baixo! Tente um número maior.")
+else:
+    print("Muito alto! Tente um número menos.")
