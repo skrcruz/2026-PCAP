e9e72f8f758d6b4600bd1677cf7f4f52fd9098a6
skrcruz <20261ctb0100038@estudantes.ifpr.edu.br>
2026-06-02 16:36:09 +0000

    adivinhe6.py

--- aula 16/adivenhe.py
+++ aula 16/adivenhe.py
@@ -6,26 +6,46 @@
 
 import random
 
-#preparamos um jogo
-numero_secreto = random.randint(1, 10)
-chances = 3
-acertou = False
+def jogar(maximo, chances):
+    numero_secreto = random.randint(1, maximo)
+    acertou = False
 
-#repetimos em quanto houver chances e não tiver acertado
-while chances > 0 and not acertou:
-    palpite = int(input("Digite um número de 1 a 10:"))
+    while chances > 0 and not acertou:
+        palpite = int(input("Seu palpite (1 a " + str(maximo) + "):"))
 
-    if palpite == numero_secreto:
-        print("Acertou!")
-        acertou = True
-    elif palpite < numero_secreto:
-        print("Muito baixo!")
-    else:
-        print("Muito alto!")
+        if palpite == numero_secreto:
+            print("Acertou!")
+            acertou = True
+        elif palpite < numero_secreto:
+            print("Muito baixo!")
+        else:
+            print("Muito alto!")
+        
+        chances = chances - 1
+        print("Chances restantes:", chances)
 
-    chances = chances - 1 #gasta uma chance
-    print("Chances restantes:", chances)
+    return acertou
 
-#quando o laço termina vemos o que aconteceu
-if not acertou:
-    print("Suas chances acabaram! O número era", numero_secreto)
+#niveis guardados em uma lista de listas
+niveis = [
+    ["Fácil", 10, 3],
+    ["Médio", 100, 5],
+    ["Impossível", 1000, 10],
+]
+
+#menu de escolha de nivel
+print("Escolha o nível de dificuldade:")
+print("1 - Fácil              (1 a 10, 3 chances)")
+print("2 - Médio              (1 a 100, 5 chances)")
+print("3 - Impossível         (1 a 1000, 10 chances)")
+opcao = int(input("Digite 1, 2 ou 3:"))
+
+#a opção 1 está na posição 0 da lista, por isso o ajuste
+nivel = niveis[opcao - 1]
+
+#iniciamos o jogo com a configuração do jogo escolhido
+print("Você escolheu o nível:", nivel[0])
+venceu = jogar(nivel[1], nivel[2])
+
+if not venceu:
+    print("Fim de jogo! Tente um nível mais fácil.")
