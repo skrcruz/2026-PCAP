d8e85399b48422ee149913e4fd7308ba3939395a
skrcruz <20261ctb0100038@estudantes.ifpr.edu.br>
2026-07-28 18:27:44 +0000

    aula_19

--- revisao/conserto1.py
+++ revisao/conserto1.py
@@ -1,6 +1,5 @@
 print("=== ADIVINHE O NUMERO ===")
 segredo = 7
-# Convertendo a entrada para número inteiro:
 palpite = int(input("Digite um numero de 1 a 10: ")) #no original falta o int que converte esse valor para inteiro
 
 if palpite == segredo:
---
+++ revisao/conserto5.py
@@ -0,0 +1,7 @@
+# Conserto 5: trecho do "Par ou Impar" (Aula 18)
+def soma_jogadas (minha, da_maquina):
+    total = minha + da_maquina
+    return total
+pontos = soma_jogadas (3, 4)
+print("A soma das jogadas foi:", pontos)
+
