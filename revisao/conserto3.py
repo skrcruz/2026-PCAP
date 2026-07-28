b5ce13a78c6576a97f71bed8b08619efaf6abcea
skrcruz <20261ctb0100038@estudantes.ifpr.edu.br>
2026-07-28 18:21:33 +0000

    aula_19

---
+++ revisao/conserto3.py
@@ -0,0 +1,6 @@
+# Conserto 3: contagem de rodadas
+contador = 1
+while contador <= 5:
+    print("Rodada", contador)
+    contador = contador + 1 # faz o loop acabar, aumentando a váriavel que sempre continuava igual
+print("fim de jogo")
