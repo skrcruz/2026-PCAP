e0c41c890aede9f203080cee4239210f69a893c0
skrcruz <20261ctb0100038@estudantes.ifpr.edu.br>
2026-07-28 18:28:59 +0000

    aula_19

--- revisao/conserto5.py
+++ revisao/conserto5.py
@@ -1,7 +1,7 @@
 # Conserto 5: trecho do "Par ou Impar" (Aula 18)
 def soma_jogadas (minha, da_maquina):
     total = minha + da_maquina
-    return total
+    return total #devolve o valor
 pontos = soma_jogadas (3, 4)
 print("A soma das jogadas foi:", pontos)
 
